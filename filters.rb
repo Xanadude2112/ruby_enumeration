08342009f1d7a6bba625fbcdd2d303e58dbef6bc
Thomas Azran <tommy.azran@gmail.com>
2024-06-20 22:24:02 +0000

    enumeration complete

--- filters.rb
+++ filters.rb
@@ -1,16 +1,64 @@
-# In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
+require 'active_support/all'
 
+# Takes in an id
+# Returns the candidate with that :id
+# If there is no candidate with that id, it naturally returns nil
 def find(id)
-    # Your code Here
+  @candidates.each do |candidate|
+    if candidate[:id] == id
+      return candidate
+    end
   end
-  
-  def experienced?(candidate)
-    # Your code Here
+  nil
+end
+
+# Takes in a single candidate (hash). Note: not the array.
+# Returns true if the candidate has 2 years of experience or more
+# Returns false otherwise
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+def github_points?(candidate)
+  candidate[:github_points] >= 100
+end
+
+def knows_ruby_or_python?(candidate)
+  languages = candidate[:languages]
+  languages.include?('Ruby') || languages.include?('Python')
+end
+
+def applied_recently?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+def age_qualified?(candidate)
+  candidate[:age] > 17
+end
+
+# Takes in a collection of candidates
+# Returns a subset of the candidates that meet the following criteria:
+# - Are experienced
+# - Have 100 or more Github points
+# - Know at least Ruby or Python
+# - Applied in the last 15 days
+# - Are over the age of 17 (18+)
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    github_points?(candidate) &&
+    knows_ruby_or_python?(candidate) &&
+    applied_recently?(candidate) &&
+    age_qualified?(candidate)
   end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
+
+# Takes in a collection of candidates
+# Instead of filtering on the candidates, returns them all back but reordered such that:
+# - Candidates with the most experience are at the top
+# - For Candidates that have the same years of experience, they are further sorted by their number of Github points (highest first)
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
+
+# More methods will go below
--- main.rb
+++ main.rb
@@ -7,3 +7,20 @@ require './filters'
 ## Your test code can go here
 
 pp @candidates
+
+# Example of using the filters
+puts "Experienced Candidates"
+experience = experienced?(@candidates)
+pp experience
+
+puts "Candidate ID"
+id = find(@candidates)
+pp id
+
+puts "Qualified Candidates:"
+qualified = qualified_candidates?(@candidates)
+pp qualified
+
+puts "Candidates Ordered by Qualifications:"
+ordered = ordered_by_qualifications?(@candidates)
+pp ordered
